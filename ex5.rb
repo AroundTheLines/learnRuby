d92904c7f3e1c91852eba3bf4206bde7010eeb2f
Qile Wang <wangqile123@gmail.com>
2015-08-17 19:17:26 -0400

    Finished example 5 study drills

--- ex5.rb
+++ ex5.rb
@@ -6,9 +6,17 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
-puts "Let's talk about #{name}"
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+def inches_to_cm (inches = height)
+	return inches * 2.54
+end
+
+def pounds_to_kg (pounds = weight)
+	return pounds * 0.453592
+end
+
+puts "Let's talk about #{name}."
+puts "He's #{inches_to_cm(height)} cm tall."
+puts "He's #{pounds_to_kg(weight)} kg heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
