52442c4cbd5f4746287dd83478c6344907e5bb04
Qile Wang <wangqile123@gmail.com>
2015-08-18 21:34:44 -0400

    finished example 16 and study drills

---
+++ ex16.rb
@@ -0,0 +1,39 @@
+filename = ARGV.first
+
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename, "r+")
+
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines."
+
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
+
+puts "Here's your file: "
+asd = target.read
+puts asd
+
+puts "And finally, we close it."
+target.close
+
+text = open(filename)
+
+puts text.read
+
+text.close
