4ed8b5104b0b72e61790b3bcd1102f4c26fb3f5a
Qile Wang <wangqile123@gmail.com>
2015-08-21 23:35:51 -0400

    finished the ex21 study drills

--- ex21.rb
+++ ex21.rb
@@ -1,21 +1,21 @@
 def add(a, b)
 	puts "ADDING #{a} + #{b}"
-	return a + b
+	a + b
 end
 
 def subtract(a, b)
 	puts "SUBTRACTING #{a} - #{b}"
-	return a - b
+	a - b
 end
 
 def multiply(a, b)
 	puts "MULTIPLYING #{a} * #{b}"
-	return a * b
+	a * b
 end
 
 def divide(a, b)
 	puts "DIVIDING #{a} / #{b}"
-	return a / b
+	a / b
 end
 
 puts "Let's do some math with just functions!"
